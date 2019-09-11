8b6544208cb862dc343899b2b6d54fda55627ddd
Leon Schoorl <leon@qbaylogic.com>
2019-09-11 17:56:24 +0200

    ...

--- SPI.hs
+++ SPI.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -Wno-unused-matches #-}
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 
 module SPI where
 import Clash.Prelude hiding (writeFile)
@@ -11,7 +13,7 @@ import Data.Text.IO  (writeFile)
 import Clash.Signal.Internal (Signal(..))
 
 type WordSize = 8
-type HalfSpiPeriod = 4
+type HalfSpiPeriod = 2
 type SpiWord = BitVector WordSize
 type ClkCounter = Index HalfSpiPeriod
 type BitCounter = Index (WordSize+1)
@@ -19,17 +21,27 @@ type BitCounter = Index (WordSize+1)
 clkIdle :: Bit
 clkIdle = 0
 
+-- topEntity = spiMaster @System
+topEntity = spiSlave @System
+
+spiSlave
+  :: HiddenClockResetEnable dom
+  => Signal dom (Maybe SpiWord)
+  -> Signal dom _
+  -> (Signal dom _, Signal dom _)
+spiSlave internal external = mealyB spiSlaveMealy slave_initState (internal,external)
 
 spiMaster
   :: HiddenClockResetEnable dom
   => Signal dom (Maybe SpiWord)
   -> "miso" ::: Signal dom Bit
   -> ( Signal dom (Maybe SpiWord)
-     , ( "sck"  ::: Signal dom Bit
-       , "mosi" ::: Signal dom Bit
-       , "ss#"  ::: Signal dom Bit)
+     , Signal dom
+       ( "sck"  ::: Bit
+       , "mosi" ::: Bit
+       , "ss#"  ::: Bit)
      )
-spiMaster wordIn miso = unbundle <$> mealyB spiMasterMealy initState (wordIn,miso)
+spiMaster wordIn miso = mealyB spiMasterMealy initState (wordIn,miso)
 
 data MasterState
   = Idle
@@ -85,6 +97,29 @@ spiMasterMealy state (inM, miso) = (state', (outM, (sck,mosi,ssN)))
       Transfering {} -> low
       _              -> high
 
+data SlaveState = SlaveState {slv_prev_clk :: Bit, slv_inw :: SpiWord, slv_outw :: SpiWord, slv_state :: SlaveState'} deriving (Generic,NFDataX)
+data SlaveState'
+  = Slave_Idle
+  | Slave_Transfering BitCounter deriving (Generic,NFDataX)
+
+slave_initState = SlaveState clkIdle 0 0 Slave_Idle
+
+spiSlaveMealy :: SlaveState -> (Maybe SpiWord,  (Bit,Bit,Bit)) -> (SlaveState, (Maybe SpiWord, Bit))
+spiSlaveMealy state (inM, (sck,mosi,ssN)) = (state', (outM, miso))
+  where
+    state' = ...
+
+    outM = Nothing
+    miso = 0
+
+
+data Change = NoChange | Rising | Falling deriving (Eq,Generic,NFDataX)
+getChange :: Bit -> Bit -> Change
+getChange prev now = case (prev,now) of
+  (0,1) -> Rising
+  (1,0) -> Fallig
+  _     -> NoChange
+
 
 
 testSpi = withClockResetEnable clockGen resetGen enableGen (spiMaster @System)
@@ -98,7 +133,7 @@ mosiIn = P.replicate 200 1
 input = P.zip testInputInternal' mosiIn
 
 testSpi2 :: Signal System (Maybe SpiWord, (Bit,Bit,Bit))
-testSpi2 = bundle $ bundle <$> testSpi testInputInternal (pure 1)
+testSpi2 = bundle $ testSpi testInputInternal (pure 1)
 
 testInput = catMaybes (fst <$> sampleN 200 testSpi2) == [0b1111_1111]
 -- geeft nu [0b0111_1111]
@@ -115,23 +150,24 @@ simulateMealyS f = go
     go _ [] = []
     go s (i:is) = let (s',o) = f s i in (s,o) : go s' is
 
+testme = putStrLn $ unlines $ fmap show $ simulateMealyS spiMasterMealy initState input
 
 
-
-miso = traceSignal1 "miso" (0 :- pure 1)
-(outw, (sck,mosi,ss)) = testSpi testInputInternal (pure 1)
-sck' = traceSignal1 "sck" sck
-mosi' = traceSignal1 "mosi" mosi
-ss' = traceSignal1 "ss#" ss
-combined = bundle (sck',mosi',ss')
 
 -- | Collect traces, and dump them to a VCD file.
 main :: IO ()
 main = do
-
-  vcd <- dumpVCD (0, 400) combined ["sck", {- "miso", -} "mosi","ss#"]
+  let
+    miso = traceSignal1 "miso" $ (pure 1)
+    (outw, unbundle -> (sck,mosi,ss)) = testSpi testInputInternal miso
+    sck' = traceSignal1 "sck" sck
+    mosi' = traceSignal1 "mosi" mosi
+    ss' = traceSignal1 "ss#" ss
+    combined = bundle (sck',mosi',ss')
+  vcd <- dumpVCD (0, 400) combined ["sck",  "miso",  "mosi", "ss#"]
   case vcd of
     Left msg ->
       error msg
-    Right contents ->
+    Right contents -> do
       writeFile "spi.vcd" contents
+      putStrLn "written output vcd file"
